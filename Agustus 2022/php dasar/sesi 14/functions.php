01e1a6f7a8b9ea820dea8fdc1f2c67c512ad71c7
Ali Abdul Rokhim <110799667+Alliabdull@users.noreply.github.com>
2022-08-26 11:00:16 +0700

    update

--- Agustus 2022/php dasar/sesi 14/functions.php
+++ Agustus 2022/php dasar/sesi 14/functions.php
@@ -58,3 +58,15 @@ function ubah($data) {
     return mysqli_affected_rows($conn);
 
 }
+
+
+function cari($keyword) {
+    $query = "SELECT * FROM mahasiswa
+                WHERE
+              nama LIKE '%$keyword%' OR
+              nrp LIKE '%$keyword%' OR
+              email LIKE '%$keyword%' OR
+              jurusan LIKE '%$keyword%' 
+            ";
+    return query($query) ;
+}
