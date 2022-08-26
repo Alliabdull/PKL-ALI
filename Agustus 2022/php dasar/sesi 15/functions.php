be8ca4844f1c9caeddd963a38dc1f21f43885a39
Ali Abdul Rokhim <110799667+Alliabdull@users.noreply.github.com>
2022-08-26 16:09:29 +0700

    update

---
+++ Agustus 2022/php dasar/sesi 15/functions.php
@@ -0,0 +1,121 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }    
+    return $rows;
+}
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+
+function tambah($data) {
+    global $conn;    
+
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    
+    // upload gambar
+    $gambar = upload();
+    if( !$gambar ) {
+        return false;
+    }
+        $query = "INSERT INTO mahasiswa
+                   VALUES
+                  ('', '$nama', '$nrp', '$email', '$jurusan',
+                     '$gambar') 
+                ";
+        mysqli_query($conn, $query);
+    
+        return mysqli_affected_rows($conn);
+    
+}
+
+
+
+function upload() {
+    
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    // cek apakah tidak ada gambar yang diupload
+    if( $error === 4 ) {
+        echo "<script>
+                alert('pilih gambar terlebih dahulu!');
+             </script>";
+        return false;
+    }
+
+
+    // cek apakah yang diupload adalah gambar
+    $ekstensiGambarValid = ['JPG', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if( !in_array($ekstensiGambar, $ekstensiGambarValid) ) {
+        echo "<script>
+               alert('yang anda upload bukan gambar!');
+            </script>";
+        return false;
+    }
+
+    // cek jika ukuranya terlalu besar
+    if( $ukuranFile > 1000000 ) {
+        echo "<script>
+                alert('ukuran gambar terlalu besar!');
+              </script>";
+        return false;
+    }
+
+    // lolos pengecekan, gambar siap diupload
+    move_uploaded_file( $tmpName, 'img/'.  $namaFile);
+
+    return $namaFile;
+        
+}
+
+
+function ubah($data) {
+    global $conn;
+
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "UPDATE mahasiswa SET
+                nama = '$nama',
+                nrp = '$nrp',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+            WHERE id = $id
+            ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+
+}
+
+
+function cari($keyword) {
+    $query = "SELECT * FROM mahasiswa
+                WHERE
+              nama LIKE '%$keyword%' OR
+              nrp LIKE '%$keyword%' OR
+              email LIKE '%$keyword%' OR
+              jurusan LIKE '%$keyword%' 
+            ";
+    return query($query) ;
+}
---
+++ Agustus 2022/php dasar/sesi 15/tambah.php
@@ -0,0 +1,64 @@
+<?php
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+
+    // var_dump($_FILES['gambar']['name']);
+    // die;
+   
+     // cek apakah data berhasil ditambah atau tidak
+     if ( tambah($_POST) > 0 ) {
+        echo "
+        <script>
+        alert('data berhasil ditambahkan!');
+        document.location.href = 'index.php';
+    </script>
+";
+    } else {
+            echo "
+                <script>
+                alert('data gagal ditambahkan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah Data Mahasiswa</h1>
+    <form action="" method="post" enctype="multipart/form-dat">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data!</button>
+            </li>
+                
+        </ul>
+
+    </form>
