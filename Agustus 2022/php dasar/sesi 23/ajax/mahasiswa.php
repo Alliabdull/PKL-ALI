72e6ff8ac3d6bb71d624f0b5c2c90d19e5ffb89e
Ali Abdul Rokhim <110799667+Alliabdull@users.noreply.github.com>
2022-08-30 11:06:04 +0700

    update

---
+++ Agustus 2022/php dasar/sesi 23/ajax/mahasiswa.php
@@ -0,0 +1,46 @@
+<?php
+usleep(500000);
+require '../functions.php';
+
+$keyword = $_GET["keyword"];
+
+$query =  "SELECT * FROM mahasiswa
+             WHERE
+           nama LIKE '%$keyword%' OR
+           nrp LIKE '%$keyword%' OR
+           email LIKE '%$keyword%' OR
+           jurusan LIKE '%$keyword%' 
+        ";
+$mahasiswa = query($query);
+?>
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <td>No.</td>
+        <td>Aksi</td>
+        <td>Gambar</td>
+        <td>NRP</td>
+        <td>Nama</td>
+        <td>Email</td>
+        <td>Jurusan</td>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php foreach( $mahasiswa as $row) : ?>
+    <tr>
+        <td><?= $i ; ?> </td>
+        <td>
+            <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+            <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="
+            return confirm('yakin?');">hapus</a>
+        </td>
+
+        <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+        <td><?= $row["nrp"]; ?></td>
+        <td><?= $row["nama"]; ?></td>
+        <td><?= $row["email"]; ?></td>
+        <td><?= $row["jurusan"]; ?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+
+</table>
